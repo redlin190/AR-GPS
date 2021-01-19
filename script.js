c5ca97d9d21f0482a31ee3360f87c84e76ce45b1
redlin190 <39657404+redlin190@users.noreply.github.com>
2021-01-20 00:20:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,19 +25,19 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        scale: '0.25 0.25 0.25',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
     {
         url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
+        scale: '0.1 0.1 0.1',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
+        scale: '0.04 0.04 0.04',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
